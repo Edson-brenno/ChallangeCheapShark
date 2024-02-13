8c268620129e1b0d09bf8f806312798935b90138
brenno <brennorebeiro@gmail.com>
2024-02-13 14:55:51 -0300

    Add architecture MVC

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ChallangeCheapShark{
+    class ChallangeCheapShark{
+        public static void Main(string[] args){
+            System.Console.WriteLine("It worked pretty well");
+        } 
+    }
+}
