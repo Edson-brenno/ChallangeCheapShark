3c797218144d116f75145a41df5ad9adf98281b0
brenno <brennorebeiro@gmail.com>
2024-02-14 13:21:47 -0300

    Add validations

--- View/MenuDealsView.cs
+++ View/MenuDealsView.cs
@@ -19,11 +19,27 @@ namespace ChallangeCheapShark.View{
                 System.Console.Write("Please, input the selected option: ");
                 string option = System.Console.ReadLine() ?? String.Empty;
                 System.Console.WriteLine($"You wrote {option}");
+                this.ValidateMenuOption(option); 
             }
             catch(Exception ex){
                 System.Console.WriteLine(ex.InnerException);
             }
             
+        }
+
+        private void ValidateMenuOption(string inputedMenuOption){
+            if (string.IsNullOrEmpty(inputedMenuOption) || string.IsNullOrWhiteSpace(inputedMenuOption)){
+                throw new NoNullMenuOptionException();
+            }
+            else if (!char.IsDigit(inputedMenuOption[0])){
+                throw new MenuOptionIsNotADigitException();
+            }
+            else if (char.IsDigit(inputedMenuOption[0]) && int.Parse(inputedMenuOption) > 3 || char.IsDigit(inputedMenuOption[0]) && int.Parse(inputedMenuOption) == 0){
+                throw new NotAValidMenuOptionException();
+            }
+            else{
+                System.Console.WriteLine("Everything is alright");
+            }
         }
         internal void Main(){
             SystemPresentationView.ShowSystemName();
